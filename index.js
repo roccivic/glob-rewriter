e6b5481baab1b9ca8fd9224de9ed6832efbb0ab8
Rouslan Placella <rouslan@placella.com>
2016-12-01 15:48:09 +0000

    Validation update

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var rewrite = require('./lib/rewrite');
 
 module.exports = {
   validate: validate,
-  rewrite: function() {
-    validate.apply(this, arguments);
-    return rewrite.apply(this, arguments);
+  rewrite: function(path, locale, source, target) {
+    validate(source, target);
+    return rewrite(path, locale, source, target);
   }
 };
